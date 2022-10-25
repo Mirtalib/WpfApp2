75e00482da22635d09bfb4a6aeaca9a3b850de31
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2022-10-25 22:53:53 +0400

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp2.Veiw;
+
+namespace WpfApp2
+{
+    public partial class MainWindow : Window
+    {
+        private List<Message> messages = new List<Message>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // txtBoxMessage
+
+            messages.Add(new Message(txtBoxMessage.Text, DateTime.Now));
+
+            Message  message = new Message(txtBoxMessage.Text, DateTime.Now);
+
+            listView.ItemsSource = null;
+            listView.ItemsSource = messages;
+            txtBoxMessage.Text = null;
+        }
+
+    }
+}
---
+++ WpfApp2/Veiw/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2.Veiw
+{
+    public class Message
+    {
+        public Message(string? context, DateTime dateTime)
+        {
+            Context = context;
+            DateTime = dateTime.ToLongTimeString();
+        }
+
+        public string? Context { get; set; }
+        public string DateTime { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"{Context} {DateTime}";
+        }
+
+    }
+}
